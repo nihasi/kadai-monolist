f9561bf42a8f3043efecabb5b05e499d0bc687e8
inawa <g16a54jbf@yahoo.co.jp>
2018-09-27 12:31:01 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users,only: [:show, :create, :new]
+  resources :users, only: [:show, :create, :new]
+  
+  resources :items, only: [:new]
 end
