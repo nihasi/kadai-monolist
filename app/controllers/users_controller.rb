6a21d36cda6b0b66a76560e2db1350711a41737d
inawa <g16a54jbf@yahoo.co.jp>
2018-09-28 13:43:31 +0000

    want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
   end
   
   private
